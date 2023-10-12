47ccf02e825c8a0338c02676b325907d1bf5973d
Mmy2000 <yahianada005@gmail.com>
2023-10-13 01:07:40 +0300

    creating product view part one

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/' , include('product.urls',namespace='product')),
 ]
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
