7cbef6af6c7db62929f720aeb8c663c77a8d1c77
Mmy2000 <yahianada005@gmail.com>
2023-10-14 12:24:12 +0300

    creating product view part five and adding pagination

--- product/views.py
+++ product/views.py
@@ -13,6 +13,8 @@ from django.db.models.query_utils import Q
 
 class ProductList(ListView):
     model = Product
+    paginate_by = 9
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
