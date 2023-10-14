35d6970f116564951a38444ded2bbe7640c25dda
Mmy2000 <yahianada005@gmail.com>
2023-10-14 15:26:14 +0300

    creating our nav part 2

--- settings/footer.py
+++ settings/footer.py
@@ -5,9 +5,9 @@ from django.db.models import Count
 
 def myfooter(request):
     myfooter = Info.objects.last()
-    categories = ProductCategory.objects.all().annotate(product_count=Count('product_category'))[:6]
+    categories_footer = ProductCategory.objects.all().annotate(product_count=Count('product_category'))[:6]
     context ={
         'myfooter':myfooter,
-        'categories':categories,
+        'categories_footer':categories_footer,
         }
     return(context)
