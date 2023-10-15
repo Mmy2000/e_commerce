630d2f8e87d1caad8daae3da43955ccb2b807eec
Mmy2000 <yahianada005@gmail.com>
2023-10-15 13:35:47 +0300

    creating our home part one

--- settings/views.py
+++ settings/views.py
@@ -3,11 +3,16 @@ from django.views.generic import ListView , DetailView
 from product.models import ProductCategory , Product
 from django.shortcuts import render
 from django.db.models.query_utils import Q
+from django.db.models import Count
+
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'settings/home.html',{})
+    categories_home = ProductCategory.objects.all().annotate(product_count=Count('product_category'))[:6]
+    return render(request, 'settings/home.html',{
+        'categories_home':categories_home,
+    })
 
 
 class CategoryList(ListView):
