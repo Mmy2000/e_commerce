5ead0c47c6f20eea8c727e9be1d82aa34f1fc4cc
Mmy2000 <yahianada005@gmail.com>
2023-10-15 13:45:11 +0300

    creating our home part two

--- settings/views.py
+++ settings/views.py
@@ -10,8 +10,13 @@ from django.db.models import Count
 # Create your views here.
 def home(request):
     categories_home = ProductCategory.objects.all().annotate(product_count=Count('product_category'))[:6]
+    product = Product.objects.all()[:8]
+    product_just_arrived = Product.objects.all().order_by('-created_at')[:8]
+
     return render(request, 'settings/home.html',{
         'categories_home':categories_home,
+        'product':product ,
+        'product_just_arrived':product_just_arrived,
     })
 
 
