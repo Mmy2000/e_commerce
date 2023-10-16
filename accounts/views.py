3353c4c29ff65c8080ea7e2e94511668a655ee65
Mmy2000 <yahianada005@gmail.com>
2023-10-16 21:00:08 +0300

    finishing home page and adding accounts app and profiles

---
+++ accounts/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render , redirect , get_object_or_404
+from .forms import SignupForm , UserForm , ProfileForm
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login 
+
+
+
+def signup(request):
+    if request.method == "POST":
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            usernames = form.cleaned_data['username']
+            passwords = form.changed_data["password1"]
+            user = authenticate(username=usernames,password=passwords)
+            login(request,user)
+
+            return redirect('/account/profile')
+            
+    else:
+        form=SignupForm()
+
+    return render(request,'registration/signup.html',{'form':form})
+
+
+
+def profile(request):
+    profile=Profile.objects.get(user=request.user)
+
+    return render(request,'profile/profile.html',{'profile':profile})
+
+
+def edit_profile(requset):
+    profile = Profile.objects.get(user=requset.user)
+    if requset.method == "POST":
+        user_form = UserForm(requset.POST , instance=requset.user)
+        profile_form = ProfileForm(requset.POST,instance=profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            myprofile = profile_form.save(commit=False)
+            myprofile.user = requset.user
+            myprofile.save()
+            return redirect('/accounts/profile')
+    else:
+        user_form = UserForm(instance=requset.user)
+        profile_form = ProfileForm(instance=profile)
+
+    return render(requset,'profile/profile_edit.html',{
+        'user_form':user_form,
+        'profile_form':profile_form
+    })
+
+
+
