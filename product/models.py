e3b36f49b03ece24f1a5f4b28be5f1fcdb943bb6
Mmy2000 <yahianada005@gmail.com>
2023-10-17 19:32:32 +0300

    adding cart page part three

--- product/models.py
+++ product/models.py
@@ -68,8 +68,6 @@ class CartItem(models.Model):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    color = models.ManyToManyField(Color)
-    size = models.ManyToManyField(Size)
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
--- product/views.py
+++ product/views.py
@@ -109,9 +109,8 @@ class ProductDetail(DetailView):
 @login_required(login_url='login')
 def add_to_cart(request, product_id):
     product = Product.objects.get(pk=product_id)
-    color = Product.objects.get()
     cart, created = Cart.objects.get_or_create(user=request.user)
-    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product , color=color)
+    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product )
     
     if not item_created:
         cart_item.quantity += 1
