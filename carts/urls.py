1420abc80ebaa15669cd768d19b3ab3b3f438308
Mmy2000 <yahianada005@gmail.com>
2023-11-22 22:18:32 +0200

    create view functionality for cart

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . views import cart 
+from . views import cart ,add_cart
 
 app_name = 'carts'
 
 urlpatterns = [
     path('', cart ,name = 'cart'),
+    path('add_cart/<int:product_id>/', add_cart ,name = 'add_cart'),
+
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from product.models import Product
+from .models import Cart , CartItem
 
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request , product_id):
+    product = Product.objects.get(id=product_id) #get the product
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+            )
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity+=1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            cart=cart,
+            quantity=1
+        )
+        cart_item.save()
+    return redirect('/cart')
 
 def cart(request):
     return render(request,'carts/cart.html')
