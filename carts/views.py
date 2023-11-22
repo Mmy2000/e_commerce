7c8a551206c1b277518fe16a5b3f45d74a656bd3
Mmy2000 <yahianada005@gmail.com>
2023-11-22 22:54:39 +0200

    create view functionality for cart part four

--- carts/views.py
+++ carts/views.py
@@ -34,16 +34,22 @@ def add_cart(request , product_id):
 
 def cart(request,total=0 ,quantity=0,cart_items=None ):
     try:
+        tax = 0
+        grand_total=0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart,is_active=True)
         for cart_item in cart_items:
             total+=(cart_item.product.price * cart_item.quantity)
             quantity+=cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total+tax
     except Cart.DoesNotExist:
         pass
     context={
         'cart_items':cart_items,
         'total':total,
-        'quantity':quantity
+        'quantity':quantity,
+        'tax':tax,
+        'grand_total':grand_total,
     }
     return render(request,'carts/cart.html',context)
