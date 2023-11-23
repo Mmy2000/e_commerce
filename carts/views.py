52ba374b566514f8110d12f5be9e747b28f8e9cc
Mmy2000 <yahianada005@gmail.com>
2023-11-23 02:48:09 +0200

    create variation functionality

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from product.models import Product
 from .models import Cart , CartItem
 from django.http import HttpResponse
+from product.models import Variation
 
 
 def _cart_id(request):
@@ -11,6 +12,16 @@ def _cart_id(request):
     return cart
 
 def add_cart(request , product_id):
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try :
+                variation = Variation.objects.get(product=product,variation_category__iexact=key , variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
     product = Product.objects.get(id=product_id) #get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
