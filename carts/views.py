ea18d1137f2ce52740ee2a9f6c3231f9794c559f
Mmy2000 <yahianada005@gmail.com>
2023-11-24 00:48:28 +0200

    create checkout page

--- carts/views.py
+++ carts/views.py
@@ -113,3 +113,29 @@ def cart(request,total=0 ,quantity=0,cart_items=None ):
         'grand_total':grand_total,
     }
     return render(request,'carts/cart.html',context)
+
+def checkout(request,total=0 ,quantity=0,cart_items=None):
+    try:
+        tax = 0
+        grand_total=0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            if cart_item.product.discount:
+                total+=(cart_item.product.discount * cart_item.quantity)
+                quantity+=cart_item.quantity
+            else:
+                total+=(cart_item.product.price * cart_item.quantity)
+                quantity+=cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total+tax
+    except Cart.DoesNotExist:
+        pass
+    context={
+        'cart_items':cart_items,
+        'total':total,
+        'quantity':quantity,
+        'tax':tax,
+        'grand_total':grand_total,
+    }
+    return render(request,'checkout/checkout.html',context)
