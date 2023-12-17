b8a4d73cae1a1c60f51cd45d765fbc802ed7caa2
Mmy2000 <yahianada005@gmail.com>
2023-12-17 20:43:50 +0200

    coupon system models

--- settings/admin.py
+++ settings/admin.py
@@ -7,6 +7,18 @@ admin.site.register(Info)
 admin.site.register(NewsLitter)
 admin.site.register(Contact)
 
+from .models import Coupons
+
+class CouponsAdmin(admin.ModelAdmin):
+	'''
+		Admin View for Coupons
+	'''
+	list_display = ('code','valid_form','valid_to','discount','active',)
+	list_filter = ('active','valid_form','valid_to',)
+	search_fields = ('code',)
+
+admin.site.register(Coupons, CouponsAdmin)
+
 
 
 
--- settings/models.py
+++ settings/models.py
@@ -58,3 +58,20 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+    
+from django.core.validators import MinValueValidator,MaxValueValidator
+# Create your models here.
+
+
+class Coupons(models.Model):
+	code 		= models.CharField(max_length=50,unique=True)
+	valid_form  = models.DateTimeField()
+	valid_to 	= models.DateTimeField()
+	discount 	= models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)]) 
+	active 		= models.BooleanField()
+	class Meta:
+		verbose_name = "Coupons"
+		verbose_name_plural = "Couponss"
+
+	def __str__(self):
+		return self.code
