e98fd7acd800493bc73059293580a68cb5e27615
Mmy2000 <yahianada005@gmail.com>
2024-05-13 09:45:49 +0300

    finishing the project

--- static/js4/index.js
+++ static/js4/index.js
@@ -1,3 +1,13 @@
 const url = window.location.href
-console.log(url);
 const searchForm = document.getElementById('searchForm')
+const searchInput = document.getElementById('searchInput')
+const results = document.getElementById('results')
+const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value
+
+searchInput.addEventListener('keyup' , (e)=>{
+    console.log(e.target.value);
+
+    if (results.classList.contains('not-visible')) {
+        results.classList.remove('not-visible')
+    }
+})
